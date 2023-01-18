6124716feea1b460c4e98fd85a371bd2816bb77a
siwaomondi <siwaomondi@gmail.com>
2023-01-18 10:05:52 +0300

    Remove greeting component,add eslint-plugin

--- src/components/Home/Intro/Intro.js
+++ src/components/Home/Intro/Intro.js
@@ -1,43 +1,14 @@
 import "../../../static/css/Intro.css";
-import { useEffect, useState } from "react";
 import Greeting from "./Greeting";
-function GreetingComponent() {
-    const [greeting, setGreeting] = useState("Hello world");
-    const greetings = [
-        "hello world",
-        "wasemaje mkuu",
-        "idhi nade",
-        "ũhana atĩa",
-        "chamgei",
-        "oriena",
-    ];
-
-    useEffect(() => {
-        var el = 0;
-        const interval = setInterval(() => {
-            setGreeting(greetings[el]);
-            el += 1;
-            if (el === greetings.length) {
-                el = 0;
-            }
-        }, 5000);
-
-        return () => clearInterval(interval);
-    }, []);
-
-    return <>{greeting}.</>;
-}
 
 function Intro() {
     return (
         <section id="intro_section">
-            <div
-                class="wrapper"
-            >
+            <div class="wrapper">
                 <span>
                     <Greeting></Greeting>
                 </span>
-                <span>I'm siwa.</span>                
+                <span>I'm siwa.</span>
             </div>
 
             <div className="intro_text">
