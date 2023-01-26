60164e47f6b37bcb2081662d12e75ac90c064aa4
siwaomondi <siwaomondi@gmail.com>
2023-01-26 21:51:01 +0300

    Modify express sttatic serve for testing

--- nodebackend.js
+++ nodebackend.js
@@ -1,6 +1,7 @@
 const PORT = 8000;
 const subdomain = require("express-subdomain");
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config;
@@ -9,9 +10,12 @@ const app = express();
 var router = express.Router();
 
 app.use(cors);
+app.use(express.static(path.join(__dirname,"build")))
 
-app.get("/", (req, res) => {
+
+app.get("/*", (req, res) => {
     res.json("hi");
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use(subdomain(process.env.REACT_APP_CUCYVIBVNKFD, router));
